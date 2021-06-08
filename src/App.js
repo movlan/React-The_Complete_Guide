db60166d4f0fb2845a78e4bc26d45ebe7fb30a1d
Bahram Movlanov <movlan@users.noreply.github.com>
2021-06-08 13:13:53 -0600

    End of Section 12

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
+
+import "./App.css";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  return <div>App</div>;
+  const [listTitle, setListTitle] = useState("My List");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
+  return (
+    <div className="app">
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+    </div>
+  );
 }
 
 export default App;
