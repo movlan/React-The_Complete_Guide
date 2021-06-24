8f70958bab15cc3bba9fe7787ea7f5fe61febcfb
Bahram Movlanov <movlan@users.noreply.github.com>
2021-06-24 16:50:30 -0600

    End of Section 22

---
+++ Section22/src/components/Profile/ProfileForm.js
@@ -0,0 +1,57 @@
+import { useContext, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
+import classes from "./ProfileForm.module.css";
+
+const API_KEY = "AIzaSyDWPXP5YQ3DL7h00pmD5i79jmTb10FQa04";
+
+const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((response) => {
+      // assumption: Always Succeeds
+      history.replace("/");
+    });
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          autoComplete="new-password"
+          ref={newPasswordInputRef}
+          minLength="7"
+        />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+};
+
+export default ProfileForm;
