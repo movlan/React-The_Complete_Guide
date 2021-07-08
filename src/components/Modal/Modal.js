d1ffaa385375ee4820c6c563bf54ca124e22131a
Bahram Movlanov <movlan@users.noreply.github.com>
2021-07-08 15:33:25 -0600

    End of Section 24

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Transition, CSSTransition } from "react-transition-group";
+
+import "./Modal.css";
+
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
+const Modal = (props) => {
+  return (
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      // classNames="fade-slide"
+      classNames={{
+        enter: "",
+        enterActive: "ModalOpen",
+        exit: "",
+        exitActive: "ModalClosed",
+      }}
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+  );
+};
+
+export default Modal;
